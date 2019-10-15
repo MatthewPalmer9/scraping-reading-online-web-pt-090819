3db1517a67e7206fcd58036f5a824be7e75b81bc
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-15 20:33:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc = Nokogiri::HTML(open("https://flatironschool.com"))
 puts element = doc.css(".headline-26OIBN")
 binding.pry
